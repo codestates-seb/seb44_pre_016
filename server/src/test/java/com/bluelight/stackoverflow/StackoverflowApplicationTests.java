563315885188416788c2f5bd1e83fa76b5286255
Lee Jungi <ljg980708@gmail.com>
2023-06-13 09:38:37 +0900

    Create bluelight server

---
+++ server/src/test/java/com/bluelight/stackoverflow/StackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluelight.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
