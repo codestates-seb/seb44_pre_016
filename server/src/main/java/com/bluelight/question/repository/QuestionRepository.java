f4888e8c5e44e3d68ebd0c7c6cb621c598ebfe37
Lee Jungi <ljg980708@gmail.com>
2023-06-16 18:52:27 +0900

    refactor: signUp

--- server/src/main/java/com/bluelight/question/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bluelight.question.repository;
-
-import com.bluelight.question.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-}
