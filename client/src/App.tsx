a3d80ebfb4ee08c5dc7945ee88db3c2970714a18
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-06-18 03:51:24 +0900

    feat: 버튼컴포넌트 & types/styled-components설치/babel-plugin-styled-components 개발의존성 설치

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,32 +6,35 @@ import { Link } from 'react-router-dom';
 import Main from './pages/Main';
 import QuestionItem from './components/QuestionItem';
 import Login from './components/login/Login';
+import Button from "./components/button/Button"
 
-const Button = styled.button`
-  border-radius: 0.3rem;
-  background-color: rgb(225, 236, 244);
-  border: 1px solid hsl(205, 41%, 63%);
-  /* text-transform: uppercase; */
-  text-transform: capitalize;
-  font-size: 13px;
-  color: rgb(57, 115, 157);
-  cursor: pointer;
-  padding: 10.5px 8px;
-`;
-const SignUp = styled(Button)`
-  background-color: rgb(10, 149, 255);
-  color: rgb(255, 255, 255);
-  font-weight: bold;
-`;
+// const Button = styled.button`
+//   border-radius: 0.3rem;
+//   background-color: rgb(225, 236, 244);
+//   border: 1px solid hsl(205, 41%, 63%);
+//   //text-transform: uppercase;
+//   text-transform: capitalize;
+//   font-size: 13px;
+//   color: rgb(57, 115, 157);
+//   cursor: pointer;
+//   padding: 10.5px 8px;
+// `;
+// const SignUp = styled(Button)`
+//   background-color: rgb(10, 149, 255);
+//   color: rgb(255, 255, 255);
+//   font-weight: bold;
+// `;
 function App() {
     return (
         <div className="App">
             <QuestionItem />
             <header>
         <Link to="/login">
-          <Button type="sigin">Log in</Button>
+          {/* <Button type="sigin">Log in</Button> */}
+          <Button variant="default">sign up</Button>
         </Link>
-        <SignUp type="sigup">sign up</SignUp>
+        {/* <SignUp type="sigup">sign up</SignUp> */}
+        <Button  />
       </header>
       <Routes>
         {/* routes 에 직접 스타일을 줄 수 있을까? */}
