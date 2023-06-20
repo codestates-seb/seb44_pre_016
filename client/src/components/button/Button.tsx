a1ed4dc2042f0bc76b3cd759891f352b0ea0c656
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-06-20 13:16:21 +0900

    refactor : 버튼

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -1,15 +1,24 @@
-import React, { ButtonHTMLAttributes, Children } from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  customStyle?: string;
 }
 
-const Button = ({ children, ...attributes }: ButtonProps) => {
+const Button = ({ children, customStyle, ...attributes }: ButtonProps) => {
+  const buttonStyles = `text-[white] bg-blue rounded-[6px]
+  inline-flex 
+  capitalize p-[10px]
+  border-[1px]
+  border-solid
+  border-[#4393F7]
+
+  hover:bg-[#0064C2]
+  active:bg-[#2960B7]
+  ${customStyle}`; // 커스텀 스타일을 포함한 클래스
+
   return (
-    <button
-      className="h-[38px] w-[100px] rounded-sm bg-sky-500 text-xs text-white hover:bg-sky-700"
-      {...attributes}
-    >
+    <button className={buttonStyles} {...attributes}>
       {children}
     </button>
   );
