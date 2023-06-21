8e6268da264f16fe7dbbff42cc786e695efabc31
withme1221 <charge1221@nate.com>
2023-06-21 10:40:00 +0900

    임시

---
+++ server/src/main/java/com/bluelight/Answer/Controller/AnswerController.java
@@ -0,0 +1,14 @@
+package com.bluelight.Answer.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/answer")
+@Validated
+@Slf4j
+public class AnswerController {
+
+}
