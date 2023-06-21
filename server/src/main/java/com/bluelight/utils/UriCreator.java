d74978ae25240970138c28c975febfb7dce718e0
Lee Jungi <ljg980708@gmail.com>
2023-06-21 12:19:28 +0900

    feat: exception 공통기능 개발

---
+++ server/src/main/java/com/bluelight/utils/UriCreator.java
@@ -0,0 +1,14 @@
+package com.bluelight.utils;
+
+import java.net.URI;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class UriCreator {
+    public static URI createUri(String defaultUrl, long resourceId) {
+        return UriComponentsBuilder
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
+    }
+}
